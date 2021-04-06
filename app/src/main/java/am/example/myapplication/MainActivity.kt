8dd7371d5fa72d175367e7fcd8f437a49ad52ba5
Ani Sahakyan <ani-sahakyan-2019@mail.ru>
2021-04-06 18:51:46 +0400

    Initial commit

---
+++ app/src/main/java/am/example/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package am.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        val image = findViewById<ImageView>(R.id.imageView)
+        image.setImageResource(R.drawable.picture)
+    }
+}
